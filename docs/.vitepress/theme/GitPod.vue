792f282661d6dccc09d8aab6cd534fc925462843
Yue Yang <g1enyy0ung@gmail.com>
2023-04-26 23:14:37 +0800

    feat: support GitPod

---
+++ docs/.vitepress/theme/GitPod.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useData } from 'vitepress'
+
+const { theme, page } = useData()
+</script>
+
+<template>
+  <a
+    :style="{
+      display: 'block',
+      marginBottom: '1rem',
+    }"
+    :href="`https://gitpod.io/#${theme.editLink.pattern.replace(':path', page.relativePath)}`"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    <img src="https://gitpod.io/button/open-in-gitpod.svg" alt="Open in Gitpod" />
+  </a>
+</template>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import GitPod from './GitPod.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'aside-top': () => h(GitPod),
+    })
+  },
+}
