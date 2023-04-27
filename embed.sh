16fced8a752a489683653648e8b1711e271a2564
daleiz <30970925+daleiz@users.noreply.github.com>
2023-04-27 15:32:44 +0800

    fix embed codes (#7)

--- embed.sh
+++ embed.sh
@@ -20,11 +20,13 @@ cp $PWD/examples/java/app/src/main/java/docs/code/examples/* $PWD/tmp/
 cp $PWD/examples/go/examples/* $PWD/tmp/
 
 npx embedme --source-root "$PWD/assets" "docs/**/quickstart-with-docker.md" $EMBED_ME_ARGS
-npx embedme --source-root "$PWD/tmp" "docs/**/guides/*.md" $EMBED_ME_ARGS
+npx embedme --source-root "$PWD/tmp" "docs/**/write/*.md" $EMBED_ME_ARGS
+npx embedme --source-root "$PWD/tmp" "docs/**/receive/*.md" $EMBED_ME_ARGS
 
 # Embed python snippets.
 if [[ "$EMBED_ME_ARGS" == "" ]]; then
-  ./include_snippets.py --snippets-root . --file-pattern "docs/**/guides/*.md"
+  ./include_snippets.py --snippets-root . --file-pattern "docs/**/write/*.md"
+  ./include_snippets.py --snippets-root . --file-pattern "docs/**/receive/*.md"
 fi
 
 rm -rf tmp
