a191784be50faae3d4cb37527c37668440f62f2f
mu <59917266+4eUeP@users.noreply.github.com>
2023-08-18 18:39:50 +0800

    Fix zookeeper version to 3.7 (#34)
    * minor improvements
    
    * Fix zookeeper version to 3.7

--- tests/start_cluster.sh
+++ tests/start_cluster.sh
@@ -2,17 +2,20 @@
 set -e
 
 docker-compose -f assets/quick-start.yaml up -d
-docker-compose -f assets/quick-start.yaml run --no-deps --rm hserver \
-  bash -c 'set -e; \
-    export timeout=120; \
-    until ( \
-      hstream --host hserver --port 6570 node check-running -n 1 && \
-      hadmin server --host hserver --port 6570 status \
-    ); do \
-      >&2 echo "Waiting for cluster..."; \
-      sleep 10; \
-      timeout=$((timeout - 10)); \
-      [ $timeout -le 0 ] && echo "Timeout!" && exit 1; \
-    done; \
-    exit 0 \
-  '
+
+export timeout=40
+until ( \
+    docker-compose -f assets/quick-start.yaml run --no-deps --rm hserver \
+        bash -c 'set -e; \
+        hstream --host hserver --port 6570 node check-running -n 1 && \
+        hadmin server --host hserver --port 6570 status'
+); do
+  >&2 echo "Waiting for cluster..."
+  sleep 10
+  timeout=$((timeout - 10))
+  if [ $timeout -le 0 ]; then
+    echo "Timeout!"
+    docker-compose -f assets/quick-start.yaml logs
+    exit 1
+  fi
+done
